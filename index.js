d4c12c8c755fd570a4f0846926dad606b3db53cd
hallx833 <38961002+hallx833@users.noreply.github.com>
2023-04-28 22:53:14 -0500

    Simplify wpm calculation

--- index.js
+++ index.js
@@ -1,58 +1,37 @@
-var charsTyped = 0;
-var charsHistory = [];
-var isCtrlDown = false;
-const wpmWindow = 5;
-const inactivityTimeout = 2;
-var secondsSinceLastKeypress = 0;
-const maxcharsHistory = wpmWindow * 10000;
-const pollingRate = 500;
+let charsTyped = 0;
+let isCtrlDown = false;
+let currentFrame = 0;
+const pollingRate = 1000;
+const wpmWindow = 5 * (1000 / pollingRate);
 
-async function onKeyDown(e) {
+function onKeyDown(e) {
     if (e.key == "Control") {
         isCtrlDown = true;
     }
     else if (!isCtrlDown && e.key.match(/^[0-9a-z]+$/)) {
         charsTyped++;
-        secondsSinceLastKeypress = 0;
     }
 }
 
-async function onKeyUp(e) {
+function onKeyUp(e) {
     if (e.key == "Control") {
         isCtrlDown = false;
     }
 }
 
-function calculateWpm(charsHistory) {
-    const charsHistoryWindow = charsHistory.slice(-wpmWindow);
-    const chars = charsHistoryWindow.reduce((a, b) => a + b, 0) / charsHistoryWindow.length;
-    return Math.round(chars / 5 * 60 * (1000 / pollingRate));
-
-}
-
-async function updateWpm() {
-    if (secondsSinceLastKeypress > inactivityTimeout) {
-        return;
-    }
-    if (charsHistory.length > maxcharsHistory) {
-        charsHistory.splice(0, charsHistory.length - wpmWindow);
-    }
-    secondsSinceLastKeypress++;
-    charsHistory.push(charsTyped);
-
-    let wpm;
-    if (charsHistory.slice(-inactivityTimeout).reduce((a, b) => a + b, 0) == 0) {
-        wpm = 0;
-    } else {
-        wpm = calculateWpm(charsHistory);
-    }
+function updateWpm() {
+    const wpm = Math.round(charsTyped / (5 * (currentFrame+1)) * 60 * (1000 / pollingRate));
     logseq.App.registerUIItem("toolbar", {
                 key: "wpm_counter", 
                 template: 
                     `<div><p style="font-size: large; opacity: 50%;">${wpm}</p></div>`
                 }
             );
-    charsTyped = 0;
+    currentFrame++;
+    if (currentFrame == wpmWindow) {
+        currentFrame = 0;
+        charsTyped = 0;
+    }
 }
 
 const main = () => {
